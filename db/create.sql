56036701b3bc9b147c83fd2aeaca8a9430378b5f
Jin Tian <today.xa@gmail.com>
2016-08-01 15:54:08 +0800

    add create.sql

---
+++ db/create.sql
@@ -0,0 +1,16 @@
+
+CREATE TABLE `t_vote` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `name` varchar(32) NOT NULL,
+ `desc` varchar(256) NOT NULL,
+ PRIMARY KEY (`id`)
+) DEFAULT CHARSET=utf8;
+
+CREATE TABLE `t_vote_item` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `vote_id` int(11) NOT NULL,
+ `option` varchar(32) NOT NULL,
+ `op_desc` varchar(256) NOT NULL,
+ `count` int(11) NOT NULL,
+ PRIMARY KEY (`id`)
+) DEFAULT CHARSET=utf8;
